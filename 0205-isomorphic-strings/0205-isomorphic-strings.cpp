3046b1d8105711932b9a2ec02f84cf9254578469
Riya Maheshwari <98450384+RiyaMaheshwari@users.noreply.github.com>
2022-10-12 19:50:43 +0530

    Time: 120 ms (5.04%), Space: 115.1 MB (5.11%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int n = s.length();
+        int m = t.length();
+        if(n != m){
+            return false;
+        }
+        
+        map<char, vector<int>> mp1;
+        map<char, vector<int>> mp2;
+        
+        for(int i=0;i<n;i++){
+            mp1[s[i]].push_back(i);
+            mp2[t[i]].push_back(i);
+        }
+        
+        for(int i=0;i<n;i++){
+            auto curr1 = mp1[s[i]];
+            auto curr2 = mp2[t[i]];
+            if(curr1 == curr2){
+                continue;
+            }
+            return false;
+        }
+        return true;
+    }
+};
