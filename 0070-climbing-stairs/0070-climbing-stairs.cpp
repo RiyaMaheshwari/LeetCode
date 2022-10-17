1e24c5f1b2f53d065d373b9046b79733050b5cd2
Riya Maheshwari <98450384+RiyaMaheshwari@users.noreply.github.com>
2022-10-18 00:21:14 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (83.22%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int climbStairs(int n) {
-       if (n <= 2) return n;
-        int prev = 2, prev2 = 1, res;
-        for (int i = 3; i <= n; i++) {
-            res = prev + prev2;
-            prev2 = prev;
-            prev = res;
-        }
-        return res;
+       if(n==1 || n==2) {
+		 return n;
+     }
+     int arr[n];
+     arr[0] = arr[1] = 1;
+        arr[2]=2;
+     for(int i = 3; i < n; i++) {
+		 arr[i] = arr[i-1] + arr[i-2];
+     }
+	 return arr[n-1] + arr[n-2];
     }
 };
