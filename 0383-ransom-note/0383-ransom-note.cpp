08eeb9ed914960ce8e6913bd927a04bc0c9389a8
Riya Maheshwari <98450384+RiyaMaheshwari@users.noreply.github.com>
2022-10-19 01:24:22 +0530

    Time: 377 ms (5.24%), Space: 8.7 MB (90.21%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-         vector<int> a(26,0),b(26,0);
-        for(auto x:magazine) 
-            b[x-'a']++;
-        for(auto x:ransomNote)
-            a[x-'a']++;
-        
-        for(int i=0;i<26;i++)
+        for(auto i:ransomNote)
         {
-            if(a[i]>b[i])
+            if(count(magazine.begin(),magazine.end(),i) <                     count(ransomNote.begin(),ransomNote.end(),i))
                 return false;
         }
-		
         return true;
     }
 };
