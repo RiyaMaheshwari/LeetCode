6f1396e8920e166f67fe52993a8641908d6d3370
Riya Maheshwari <98450384+RiyaMaheshwari@users.noreply.github.com>
2022-10-19 01:25:46 +0530

    Update 0383-ransom-note.cpp

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -3,7 +3,7 @@ public:
     bool canConstruct(string ransomNote, string magazine) {
         for(auto i:ransomNote)
         {
-            if(count(magazine.begin(),magazine.end(),i) <                     count(ransomNote.begin(),ransomNote.end(),i))
+            if(count(magazine.begin(),magazine.end(),i) <count(ransomNote.begin(),ransomNote.end(),i))
                 return false;
         }
         return true;
