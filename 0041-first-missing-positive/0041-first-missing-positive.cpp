a89aeb864b469d7fd20106bdd343ad6c9535411c
Riya Maheshwari <98450384+RiyaMaheshwari@users.noreply.github.com>
2023-01-29 04:47:53 +0530

    Time: 58 ms (58.04%), Space: 31.4 MB (85.08%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -3,19 +3,31 @@ public:
     int firstMissingPositive(vector<int>& nums) {
             // using extra space
             int n=nums.size();
-            unordered_map<int, int>freq;
+            // unordered_map<int, int>freq;
+            // for(int i=0; i<n; i++)
+            // {
+            //         freq[nums[i]]++;
+            // }
+            // int max = *max_element (nums.begin(), nums.end());
+            // if(max<0)
+            //         return 1;
+            // for(int i=1; i<max; i++)
+            // {
+            //         if(freq[i]==0)
+            //                 return i;
+            // }
+            // return max+1;
+            int c=1;
+            sort(nums.begin(), nums.end());
             for(int i=0; i<n; i++)
             {
-                    freq[nums[i]]++;
+                    if(nums[i]<c)
+                            continue;
+                    if(nums[i]==c)
+                            c++;
+                    else 
+                            return c;
             }
-            int max = *max_element (nums.begin(), nums.end());
-            if(max<0)
-                    return 1;
-            for(int i=1; i<max; i++)
-            {
-                    if(freq[i]==0)
-                            return i;
-            }
-            return max+1;
+            return c;
     }
 };
