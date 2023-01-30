a5e16ae37c9297c093993e5501d0c4bffaa22452
Riya Maheshwari <98450384+RiyaMaheshwari@users.noreply.github.com>
2023-01-31 04:39:43 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (94.31%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+            if(n==0)
+                    return n;
+            if(n==1 || n==2)
+                    return 1;
+           
+            int a[n+1];
+            a[0]=0, a[1]=1, a[2]=1;
+            for(int i=3; i<=n; i++)
+            {
+                    a[i]=a[i-1]+a[i-2]+a[i-3];
+            }
+            return a[n];
+    }
+};
