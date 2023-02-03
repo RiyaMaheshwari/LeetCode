284fae4613fc1a228dc3bd9df58c79aa23cb55c9
Riya Maheshwari <98450384+RiyaMaheshwari@users.noreply.github.com>
2023-02-03 23:32:42 +0530

    Time: 7 ms (60.98%), Space: 7.9 MB (22.26%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,107 +1,107 @@
-// class Solution {
-// public:
-//      bool validChar(char ch){
-//         if((ch>='a' && ch<='z')||(ch>='A' && ch<='Z')||(ch>='0' && ch<='9')){
-//             return 1;
-//         }
-//         return 0;
-//     }   
-//     char toLowerCase(char ch){
-//         if((ch>='a' && ch<='z')||(ch>='0' && ch<='9'))
-//             return ch;
-//         else{
-//             char temp = ch-'A'+'a';
-//             return temp;
-//         }
-//     }
-//         bool check(string a){
-//         int s=0;
-//         int e=a.size()-1;
-//         while(s<=e){
-//             if(a[s]!=a[e])
-//                 return 0;
-//             else{
-//                 s++;
-//                 e--;
-//             }
-//         }
-//         return 1;
-//         }
-                
-//         bool isPalindrome(string s) {
-//         //faltu char hta diya
-//         string temp="";
-//         for(int i=0; i<s.length(); i++)
-//         {
-//             if(validChar(s[i])){
-//                 temp.push_back(s[i]);
-//             }
-//         }
-//         //lowerCase 
-//         for(int i=0; i<s.length(); i++){
-//             temp[i]=toLowerCase(temp[i]);
-//         }
-//         return check(temp);
-//     }
-// };
-
-#include<string>
-#include<iostream>
-#include<vector>
-using namespace std;
 class Solution {
-private:
-    bool valid(char ch) {
-        if( (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9')) {
+public:
+     bool validChar(char ch){
+        if((ch>='a' && ch<='z')||(ch>='A' && ch<='Z')||(ch>='0' && ch<='9')){
             return 1;
         }
-        
         return 0;
-    }
-    
-    char toLowerCase(char ch) {
-    if( (ch >='a' && ch <='z') || (ch >='0' && ch <='9') )
-        return ch;
-    else{
-        char temp = ch - 'A' + 'a';
-        return temp;
-    }
-}
-    bool checkPalindrome(string a) {
-    int s = 0;
-    int e = a.length()-1;
-
-    while(s<=e) {
-        if(a[s] != a[e])
-        {
-            return 0;       
-        }
+    }   
+    char toLowerCase(char ch){
+        if((ch>='a' && ch<='z')||(ch>='0' && ch<='9'))
+            return ch;
         else{
-            s++;
-            e--;
+            char temp = ch-'A'+'a';
+            return temp;
         }
     }
-    return 1;
-}
-public:
-    bool isPalindrome(string s) {
-        
-        //faltu character hatado
-        string temp = "";
-        
-        for(int j=0; j<s.length(); j++) {   
-            if(valid(s[j])) {
-                temp.push_back(s[j]);
+        bool check(string a){
+        int s=0;
+        int e=a.length()-1;
+        while(s<=e){
+            if(a[s]!=a[e])
+                return 0;
+            else{
+                s++;
+                e--;
             }
         }
-        
-        //lowercase me kardo
-        for(int j=0; j<temp.length(); j++) { 
-            temp[j] = toLowerCase(temp[j]);
+        return 1;
+    }
+                
+        bool isPalindrome(string s) {
+        //faltu char hta diya
+        string temp = "";
+        for(int i=0; i<s.length(); i++)
+        {
+            if(validChar(s[i])){
+                temp.push_back(s[i]);
+            }
         }
-        
-        //check palindrome
-        return checkPalindrome(temp);
-        
+        //lowerCase 
+        for(int i=0; i<temp.length(); i++){
+            temp[i]=toLowerCase(temp[i]);
+        }
+        return check(temp);
     }
 };
+
+// #include<string>
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// class Solution {
+// private:
+//     bool valid(char ch) {
+//         if( (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9')) {
+//             return 1;
+//         }
+        
+//         return 0;
+//     }
+    
+//     char toLowerCase(char ch) {
+//     if( (ch >='a' && ch <='z') || (ch >='0' && ch <='9') )
+//         return ch;
+//     else{
+//         char temp = ch - 'A' + 'a';
+//         return temp;
+//     }
+// }
+//     bool checkPalindrome(string a) {
+//     int s = 0;
+//     int e = a.length()-1;
+
+//     while(s<=e) {
+//         if(a[s] != a[e])
+//         {
+//             return 0;       
+//         }
+//         else{
+//             s++;
+//             e--;
+//         }
+//     }
+//     return 1;
+// }
+// public:
+//     bool isPalindrome(string s) {
+        
+//         //faltu character hatado
+//         string temp = "";
+        
+//         for(int j=0; j<s.length(); j++) {   
+//             if(valid(s[j])) {
+//                 temp.push_back(s[j]);
+//             }
+//         }
+        
+//         //lowercase me kardo
+//         for(int j=0; j<temp.length(); j++) { 
+//             temp[j] = toLowerCase(temp[j]);
+//         }
+        
+//         //check palindrome
+//         return checkPalindrome(temp);
+        
+//     }
+// };
