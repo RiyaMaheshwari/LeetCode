81e0aa113099bf16f2c867618b9a406bb9a94639
Riya Maheshwari <98450384+RiyaMaheshwari@users.noreply.github.com>
2023-02-16 02:06:10 +0530

    Time: 16 ms (77.30%), Space: 13.7 MB (51.87%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,22 +1,44 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int,int>mp;
-        vector<pair<int, int>>p;
-        for(int i=0; i<nums.size(); i++)
-        {
-            mp[nums[i]]++;
-        }
-        for(auto x: mp)
-        {
-            p.push_back({x.second, x.first});
-        }
-        sort(p.rbegin(), p.rend());
-        vector<int>ans(k);
-        for(int i=0; i<k; i++)
-        {
-            ans[i]=p[i].second;
-        }
-        return ans;
+            // normal approach
+        // unordered_map<int,int>mp;
+        // vector<pair<int, int>>p;
+        // for(int i=0; i<nums.size(); i++)
+        // {
+        //     mp[nums[i]]++;
+        // }
+        // for(auto x: mp)
+        // {
+        //     p.push_back({x.second, x.first});
+        // }
+        // sort(p.rbegin(), p.rend());
+        // vector<int>ans(k);
+        // for(int i=0; i<k; i++)
+        // {
+        //     ans[i]=p[i].second;
+        // }
+        // return ans;
+            
+            // using heap
+            unordered_map<int, int>mp;
+            priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>pq;
+            vector<int>ans;
+            for(int i=0; i<nums.size(); i++)
+            {
+                    mp[nums[i]]++;
+            }
+            for(auto x:mp)
+            {
+                   pq.push({x.second,x.first});
+                    if(pq.size()>k)
+                            pq.pop();
+            }
+            while(pq.size()!=0)
+            {
+                  ans.push_back(pq.top().second);
+                    pq.pop();
+            }
+            return ans;
     }
 };
