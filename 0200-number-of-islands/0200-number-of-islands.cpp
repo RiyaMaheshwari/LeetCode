a3bb5312c6905493379d2e90d53ceaca42db0631
Riya Maheshwari <98450384+RiyaMaheshwari@users.noreply.github.com>
2023-03-07 01:40:57 +0530

    Time: 43 ms (44.27%), Space: 19.3 MB (13.20%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,32 +1,6 @@
 class Solution {
-        private:
-        void bfs(int row, int col, vector<vector<int>>&vis, vector<vector<char>>&grid, int delrow[], int delcol[])
-        {
-                vis[row][col]=1;
-                queue<pair<int,int>>q;
-                q.push({row,col});
-                 int n = grid.size();
-                 int m = grid[0].size();
-                while(!q.empty())
-                {
-                     int row=q.front().first;
-                     int col=q.front().second;
-                        q.pop();
-                        for(int i=0; i<4; i++)
-                        {
-                                int nrow=row+delrow[i];
-                                int ncol=col+delcol[i];
-                                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]=='1' && !vis[nrow][ncol])
-                                {
-                                        vis[nrow][ncol]=1;
-                                        q.push({nrow,ncol});
-                                }
-                        }
-                }
-                
-        }
         public:
-        void bfs(int row, int col, vector<vector<int>>&vis, vector<vector<int>>&grid, int delrow[], int delcol[])
+        void bfs(int row, int col, vector<vector<int>>&vis, vector<vector<char>>&grid, int delrow[], int delcol[])
         {
                 vis[row][col]=1;
                 queue<pair<int,int>>q;
