42b8dbf2b7661f46014a1876d858c9f7a43d7887
Riya Maheshwari <98450384+RiyaMaheshwari@users.noreply.github.com>
2023-04-08 15:38:53 +0530

    Time: 142 ms (43.16%), Space: 71.3 MB (88.20%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
@@ -1,25 +1,14 @@
 class Solution {
 public:
-    typedef long long LONG;
-
-    bool isValid(vector<int> &nums, int k) {
-        if (nums[0] > k) return false;
-        LONG prev = nums[0];
-        for (int i = 1; i < nums.size(); ++i) {
-            LONG d = k - prev;
-            prev = nums[i] - d;
-            if (prev > k) return false;
+    int minimizeArrayValue(vector<int>& nums) {
+         long long ans = 0, prefixSum = 0;
+        
+       
+        for (int i = 0; i < nums.size(); ++i) {
+            prefixSum += nums[i];
+            ans = max(ans, (prefixSum + i) / (i + 1));
         }
-        return true;
-    }
-
-    int minimizeArrayValue(vector<int> &nums) {
-        int res, left = 0, right = *max_element(nums.begin(), nums.end());
-        while (left <= right) {
-            int mid = (right - left) / 2 + left;
-            if (isValid(nums, mid)) res = mid, right = mid - 1;
-            else left = mid + 1;
-        }
-        return res;
+        
+        return ans;
     }
 };
